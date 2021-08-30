4b3f2914902e54eb65b25d66f3c706ca87a243f4
Jessie Wang <jwang329@mit.edu>
2021-08-30 19:14:46 -0400

    linked home page again

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,33 +16,7 @@ class App extends Component {
   // makes props available in this component
   constructor(props) {
     super(props);
-    this.state = {
-      userId: undefined,
-    };
   }
-
-  componentDidMount() {
-    get("/api/whoami").then((user) => {
-      if (user._id) {
-        // they are registed in the database, and currently logged in.
-        this.setState({ userId: user._id });
-      }
-    });
-  }
-
-  handleLogin = (res) => {
-    console.log(`Logged in as ${res.profileObj.name}`);
-    const userToken = res.tokenObj.id_token;
-    post("/api/login", { token: userToken }).then((user) => {
-      this.setState({ userId: user._id });
-      post("/api/initsocket", { socketid: socket.id });
-    });
-  };
-
-  handleLogout = () => {
-    this.setState({ userId: undefined });
-    post("/api/logout");
-  };
 
   render() {
     return (
